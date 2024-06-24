02610f3e68ea755e29450f99ca6e7a16ad082b55
KimHyunLo <leeas016@gmail.com>
2024-06-24 18:36:03 +0900

    refactor: 이미지 webp 확장자 추가

--- src/components/LazyImage.jsx
+++ src/components/LazyImage.jsx
@@ -1,6 +1,19 @@
 function LazyImage({ src, alt, className, loading = 'lazy' }) {
+  const isIcon = src.includes('.svg')
+  const fileName = src.split('.')[0]
+
   return (
-    <img src={src} alt={alt} className={className} loading={loading} height="300px" width="300px" />
+    <picture>
+      {isIcon || <source srcSet={`webp/${fileName}.webp`} type="image/webp" />}
+      <img
+        src={src}
+        alt={alt}
+        className={className}
+        loading={loading}
+        height="300px"
+        width="300px"
+      />
+    </picture>
   )
 }
 
